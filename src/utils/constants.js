91aa89aca72df606db3276f91e46af6a74284046
Anthony <anthony@vitex.asia>
2025-09-08 11:52:41 +0700

    123

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
 // API Base URL
-export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'https://winhouse.id.vn/api';
+export const API_BASE_URL ='https://backend.winhouse.id.vn/api';
 
 // Các hằng số cho các game
 export const GAME_CONFIG = {
